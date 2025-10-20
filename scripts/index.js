cc068b0a34e8115d96b21353a827ce3918bf7537
Nils Roberts Krištopāns <A230756NK@rvt.lv>
2025-10-20 10:19:23 +0300

    Initial commit.

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const themeToggle = document.getElementById('themeToggle')
+const themeIcon = themeToggle.querySelector('i')
+const burger = document.getElementById('burger')
+const navMenu = document.getElementById('navMenu')
+const navLinks = navMenu.querySelectorAll('a')
+
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode')
+    if (document.body.classList.contains('dark-mode')) {
+        themeIcon.classList.remove('fa-moon')
+        themeIcon.classList.add('fa-sun')
+    } else {
+        themeIcon.classList.remove('fa-sun')
+        themeIcon.classList.add('fa-moon')
+    }
+})
+
+burger.addEventListener('click', () => {
+    navMenu.classList.toggle('active')
+    themeToggle.style.display = navMenu.classList.contains('active') ? 'block' : 'none'
+})
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navMenu.classList.remove('active')
+        themeToggle.style.display = 'none'
+    })
+})
